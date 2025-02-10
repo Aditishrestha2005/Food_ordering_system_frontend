73d43c2ef822ae64aa3153bb756e43e1c52ebdcb
aditi <adishrestha2005@gmail.com>
2025-02-10 14:01:35 +0545

    dashboard updated

--- src/components/private/Dashboard.jsx
+++ src/components/private/Dashboard.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import { DashboardList } from "../helpers/dashboardList";
+import '../private/dashboard.css';
 const DashboardPage = () => {
+ 
     return (
+        
         <div className="dashboard">
             <h1 className="dashboardTitle">Our Dashboard</h1>
-            <div className="dashboardList">
+            <div className="dashboardListContainer">
+                <ul><li>asbdhasj</li></ul>
+            </div>
+            <div className="dashboardList">{DashboardList.map((DahboardItem,key)=>{
+                return <div></div>;
+            })}
             </div>
         </div>
     );
